dd2004666ec186d0aec2dd4f15bff36cc73faabf
dividead <dividead@users.noreply.github.com>
2014-06-23 15:12:28 +0400

    Update anagram.rb

--- anagram.rb
+++ anagram.rb
@@ -1,9 +1,9 @@
-a= %w(ruby ubyr python thonyp xyz zxe).map{|i| i.split(//)}
+a = %w(ruby ubyr python thonyp xyz zxe).map{|i| i.split(//)}
 @c = []
 def anagram(a)
   if a.size > 1
     x = a.shift
-    a.each{|i| @c<<[i.join,x.join] and a.delete i if i.sort == x.sort}
+    a.each{|i| @c << [i.join,x.join] and a.delete(i) if i.sort == x.sort}
     anagram(a)
   else
     @c
